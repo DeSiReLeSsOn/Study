58bfce77fc2be90c83c407f6c34fb663440445b7
DeSiReLeSsOn <theproblemdi@gmail.com>
2023-08-24 16:22:33 +0300

    .

--- django_project/bookmarks/account/views.py
+++ django_project/bookmarks/account/views.py
@@ -2,6 +2,12 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def dashboard(request,
+              'account/dashboard.html',
+              {'section': 'dashboard'})
 
 
 
