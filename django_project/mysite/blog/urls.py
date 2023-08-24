c0946ee413767745025e9f6c307b7811c9b34474
desirelesson <theproblemdi@gmail.com>
2023-08-24 12:41:19 +0300

    .

--- django_project/mysite/blog/urls.py
+++ django_project/mysite/blog/urls.py
@@ -17,4 +17,5 @@ urlpatterns = [
     path('<int:post_id>/comment/',
          views.post_comment, name='post_comment'),
     path('feed/', LatestPostsFeed(), name='post_feed'),
+    path('search/', views.post_search, name='post_search'),
 ]
--- django_project/mysite/blog/views.py
+++ django_project/mysite/blog/views.py
@@ -9,7 +9,9 @@ from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
 from django.db.models import Count
-
+from django.contrib.postgres.search import SearchVector, \
+                                           SearchQuery, SearchRank
+from django.contrib.postgres.search import TrigramSimilarity
 
 
 def post_search(request):
@@ -21,10 +23,13 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
+            search_vector = SearchVector('title', weight='A') + \
+                            SearchVector('body', weight='B')
+            search_query = SearchQuery(query)
             results = Post.published.annotate(
-                search=SearchVector('title', 'body'),
-            ).filter(search=query)
-
+                similarity=TrigramSimilarity('title',query),
+            ).filter(similarity__gt=0.1).order_by('-similarity')
+                
     return render(request,
                   'blog/post/search.html',
                   {'form': form,
