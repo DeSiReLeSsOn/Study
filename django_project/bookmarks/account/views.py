6225abcc8100c12977f8a3c1ff7a16ac6861ead8
DeSiReLeSsOn <theproblemdi@gmail.com>
2023-08-29 15:14:32 +0300

    .

--- django_project/bookmarks/account/views.py
+++ django_project/bookmarks/account/views.py
@@ -1,10 +1,27 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm
 from django.contrib.auth.decorators import login_required
 
 
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(
+                user_form.cleaned_data['password'])
+            new_user.save()
+            return render(request,
+                          'account/register_done.html',
+                          {'new_user':new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request,
+                  'account/register.html',
+                  {'user_form':user_form})
 
 @login_required
 def dashboard(request):
