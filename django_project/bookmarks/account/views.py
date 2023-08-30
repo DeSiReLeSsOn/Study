75b71849ff585daf27b9908d5f24fded6e698af5
desirelesson <theproblemdi@gmail.com>
2023-08-30 16:10:31 +0300

    add edit profile

--- django_project/bookmarks/account/views.py
+++ django_project/bookmarks/account/views.py
@@ -1,8 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm, UserRegistrationForm
+from .forms import LoginForm, UserRegistrationForm, \
+                   UserEditForm, ProfileEditForm
 from django.contrib.auth.decorators import login_required
+from .models import Profile
+
+
+        
 
 
 
@@ -14,6 +19,7 @@ def register(request):
             new_user.set_password(
                 user_form.cleaned_data['password'])
             new_user.save()
+            Profile.objects.create(user=new_user)
             return render(request,
                           'account/register_done.html',
                           {'new_user':new_user})
@@ -52,3 +58,30 @@ def user_login(request):
     else:
         form = LoginForm()
     return render(request, 'account/login.html', {'form': form})
+
+
+
+
+
+
+@login_required
+def edit(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(instance=request.user,
+                                 data=request.POST)
+        profile_form = ProfileEditForm(
+                                    instance=request.user.profile,
+                                    data=request.POST,
+                                    files=request.FILES)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(
+                                        instance=request.user.profile)
+    return render(request,
+                  'account/edit.html',
+                  {'user_form': user_form,
+                  'profile_form': profile_form})
+            
--- django_project/bookmarks/bookmarks/settings.py
+++ django_project/bookmarks/bookmarks/settings.py
@@ -130,4 +130,10 @@ LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
 
 
+
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
