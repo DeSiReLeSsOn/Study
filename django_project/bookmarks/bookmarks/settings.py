c1e927138234138eff30ba8096060352955a9d5d
desirelesson <theproblemdi@gmail.com>
2023-09-11 17:30:03 +0300

    create image-list

--- django_project/bookmarks/bookmarks/settings.py
+++ django_project/bookmarks/bookmarks/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from django.urls import reverse_lazy
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -42,6 +43,8 @@ INSTALLED_APPS = [
     "social_django",
     "django_extensions",
     "images.apps.ImagesConfig",
+    "easy_thumbnails",
+
 
 
 ]
@@ -169,7 +172,11 @@ SOCIAL_AUTH_PIPELINE = [
 ]
 
 
-if DEBUG:
- import mimetypes
- mimetypes.add_type('application/javascript', '.js', True)
- mimetypes.add_type('text/css', '.css', True)
+
+
+
+
+ABSOLUTE_URL_OVERRIDES = {
+ 'auth.user': lambda u: reverse_lazy('user_detail',
+                                     args=[u.username])
+}
--- django_project/bookmarks/images/urls.py
+++ django_project/bookmarks/images/urls.py
@@ -8,5 +8,8 @@ urlpatterns = [
     path('create/', views.image_create, name='create'),
     path('detail/<int:id>/<slug:slug>/',
          views.image_detail, name='detail'),
+    path('like/', views.image_like, name='like'),
+    path('', views.image_list, name='list'),
+
     
 ]
