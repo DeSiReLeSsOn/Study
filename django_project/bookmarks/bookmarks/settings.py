edc7c8a94ce1e5d4c057b1ba11d93c201c887c5a
desirelesson <theproblemdi@gmail.com>
2023-09-17 01:14:59 +0300

    .

--- django_project/bookmarks/bookmarks/settings.py
+++ django_project/bookmarks/bookmarks/settings.py
@@ -45,12 +45,11 @@ INSTALLED_APPS = [
     "images.apps.ImagesConfig",
     "easy_thumbnails",
     "actions.apps.ActionsConfig",
-
-
-
+    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -181,3 +180,8 @@ ABSOLUTE_URL_OVERRIDES = {
  'auth.user': lambda u: reverse_lazy('user_detail',
                                      args=[u.username])
 }
+
+
+INTERNAL_IPS = [
+ '127.0.0.1',
+]
