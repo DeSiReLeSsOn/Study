133dff2bef1c434090f58c64d688cb4becd81a92
desirelesson <theproblemdi@gmail.com>
2023-09-20 02:16:47 +0300

    create cart.py and cart views

--- django_project/myshop/myshop/settings.py
+++ django_project/myshop/myshop/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "shop.apps.ShopConfig",
+    "cart.apps.CartConfig",
+
 ]
 
 MIDDLEWARE = [
@@ -127,3 +129,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+
+CART_SESSION_ID = 'cart'
