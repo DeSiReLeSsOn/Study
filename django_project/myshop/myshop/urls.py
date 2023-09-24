a6d21aacdafd8e9d32960ee0459c3fdcbf68a2ad
desirelesson <theproblemdi@gmail.com>
2023-09-24 18:55:56 +0300

    create orders app

--- django_project/myshop/myshop/urls.py
+++ django_project/myshop/myshop/urls.py
@@ -19,8 +19,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
     path('cart/', include('cart.urls', namespace='cart')),
+    path('orders/', include('orders.urls', namespace='orders')),
     path('', include('shop.urls', namespace='shop')),
 ]
 
