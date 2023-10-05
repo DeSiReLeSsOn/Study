708650755c2860d328cebd969aedd18620522628
desirelesson <theproblemdi@gmail.com>
2023-10-05 23:57:37 +0300

    .

--- django_project/myshop/myshop/settings.py
+++ django_project/myshop/myshop/settings.py
@@ -135,3 +135,6 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 
 CART_SESSION_ID = 'cart'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
--- django_project/myshop/orders/views.py
+++ django_project/myshop/orders/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import OrderItem 
 from .forms import OrderCreateForm
 from cart.cart import Cart 
+from .tasks import order_created
 
 
 def order_create(request):
@@ -16,6 +17,7 @@ def order_create(request):
                                          price=item['price'],
                                          quantity=item['quantity'])
             cart.clear()
+            order_created.delay(order.id)
             return render(request,
                           'orders/order/created.html',
                           {'order': order})
