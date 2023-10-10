3ea7dd493176692d3d927a13db451f3603f6f31f
DeSiReLeSsOn <theproblemdi@gmail.com>
2023-10-10 23:58:07 +0300

    add in settings yandex.kassa

--- django_project/myshop/orders/tasks.py
+++ django_project/myshop/orders/tasks.py
@@ -8,7 +8,7 @@ def order_created(order_id):
     successfully created. """
     order = Order.objects.get(id=order_id)
     subject = f'Order nr. {order.id}'
-    message = f'Dear {order.firs_name},\n\n' \
+    message = f'Dear {order.first_name},\n\n' \
               f'You have successfully placed an order.' \
               f'Your order ID is {order.id}.'
     mail_sent = send_mail(subject, message, 'admin@myshop.com', [order.email])
