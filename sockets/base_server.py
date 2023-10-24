c6956df202e0f19a801aee2ac0fe442bfcfd7963
desirelesson <theproblemdi@gmail.com>
2023-10-24 15:50:16 +0300

    updated simple_server

--- sockets/base_server.py
+++ sockets/base_server.py
@@ -2,17 +2,40 @@ import socket
 
 
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(('127.0.0.1', 2000))
+
 
 #server = socket.create_sever(('127.0.0.1', 2000)) 
+def start_serv():
+    try:
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.bind(('127.0.0.1', 2000))
+        server.listen(4)
+        while True:
+            print('working...')
+            client_socket, address = server.accept()
+            data = client_socket.recv(1024).decode('utf-8')
+            #print(data)
+            
+            content = load_page_from_get_request(data)
+            client_socket.send(content)
+            client_socket.shutdown(socket.SHUT_WR)
+    except KeyboardInterrupt:
+        server.close()
 
-server.listen(4)
-print('working...')
-client_socket, address = server.accept()
-data = client_socket.recv(1024).decode('utf-8')
-print(data)
-HDRS = 'HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\n\r\n'
-content = 'Well done, great job buddy...'.encode('utf-8')
-client_socket.send(HDRS.encode('utf-8') + content)
-print('some text')
+
+def load_page_from_get_request(request_data):
+    HDRS = 'HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\n\r\n'
+    HDRS_404 = 'HTTP/1.1 404 OK\r\nContent-Type: text/html; charset=utf-8\r\n\r\n'
+    path = request_data.split(' ')[1]
+    response = ''
+    try:
+        with open('views'+path, 'rb') as file:
+            response = file.read()
+        return HDRS.encode('utf-8') + response
+    except FileNotFoundError:
+        return (HDRS_404 + 'Page Not Found...').encode('utf-8')
+
+
+
+if __name__ == '__main__':
+    start_serv()
