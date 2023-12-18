9580d695b640d72433e39671da80542872bfb0da
desirelesson <theproblemdi@gmail.com>
2023-12-18 11:56:58 +0300

    soluted task

--- test.py
+++ test.py
@@ -1,24 +1,18 @@
-res = []
-flag = True
-s = '{]'
-for i in s:
-    if i in '([{':
-        res.append(i)
-    elif i in ')]}':
-        if not res:
-            flag = False 
-            break 
-        b = res.pop()
-        if b == '(' and i == ')':
-            continue
-        if b == '[' and i == ']':
-            continue
-        if b == '{' and i == '}':
-            continue 
-        flag = False
-        break 
-
-if flag and len(res) == 0:
-    print('y')
-else:
-    print('n')
+def two_sum(a,b):
+    d = ListNode()
+    curr_node = d 
+    carry = 0 
+    while a or b:
+        x = a.val if a else 0
+        y = b.val if b else 0
+        curr_sum = carry + x + y 
+        carry = curr_sum // 10 
+        curr_node.next = ListNode(curr_sum%10) 
+        curr_node = curr_node.next 
+        if a:
+            a.next 
+        if b:
+            b.next 
+        if carry != 0:
+            curr_node.next = ListNode(carry) 
+    return d.next  
