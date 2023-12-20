762495fa4940694b5c45f9204619ee088a928068
desirelesson <theproblemdi@gmail.com>
2023-12-20 13:58:31 +0300

    solution algorithms

--- test.py
+++ test.py
@@ -1,18 +1,16 @@
-def two_sum(a,b):
-    d = ListNode()
-    curr_node = d 
-    carry = 0 
-    while a or b:
-        x = a.val if a else 0
-        y = b.val if b else 0
-        curr_sum = carry + x + y 
-        carry = curr_sum // 10 
-        curr_node.next = ListNode(curr_sum%10) 
-        curr_node = curr_node.next 
-        if a:
-            a.next 
-        if b:
-            b.next 
-        if carry != 0:
-            curr_node.next = ListNode(carry) 
-    return d.next  
+def no_repeat(s, target):
+    start = 0 
+    end = 0 
+    maxLen = 0
+    res = set() 
+    while end < len(s):
+        if  s[end] not in res:
+            res.add(s[end]) 
+            end += 1
+            maxLen = max(maxLen, end - start)
+        else:
+            res.remove([s[start]]) 
+            start += 1
+    return maxLen
+
+    
