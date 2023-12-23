beee26c95163e7b9f2ea056d3dbb4919b04c40d2
desirelesson <theproblemdi@gmail.com>
2023-12-23 16:11:21 +0300

    algorithms

--- test.py
+++ test.py
@@ -1,16 +1,32 @@
-def no_repeat(s, target):
+def binary_search(s):
     start = 0 
     end = 0 
-    maxLen = 0
-    res = set() 
+    maxLen = 0 
+    res = set()
     while end < len(s):
-        if  s[end] not in res:
-            res.add(s[end]) 
+        if s[end] not in set:
+            res.add(s[end])
             end += 1
-            maxLen = max(maxLen, end - start)
+            maxLen = max(maxLen, end - start) 
         else:
-            res.remove([s[start]]) 
-            start += 1
+            res.remove(s[start])
+            start += 1 
     return maxLen
 
+   
+  
+
+
+   
     
+    
+    
+            
+
+
+a = [2,7,11,15]
+b = 15
+print(binary_search(a, b))
+
+
+
