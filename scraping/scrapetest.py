a11c4277cb8d008cb07cef346c8fa443ef541b61
DeSiReLeSsOn <theproblemdi@gmail.com>
2024-03-17 21:16:09 +0300

    first scraping lesson

--- scraping/scrapetest.py
+++ scraping/scrapetest.py
@@ -1,11 +1,23 @@
-"""from urllib.request import urlopen
-html = urlopen('http://pythonscraping.com/pages/page1.html')
-print(html.read())"""
-
 from urllib.request import urlopen
+from urllib.error import HTTPError
 from bs4 import BeautifulSoup
 
-html = urlopen('http://www.pythonscraping.com/pages/page1.html')
-#bs = BeautifulSoup(html.read(), 'html.parser')
-bs = BeautifulSoup(html, 'html.parser')
-print(bs.h1)
+
+def getTitle(url):
+    try:
+        html = urlopen(url)
+    except HTTPError as e:
+        return None
+    try:
+        bsObj = BeautifulSoup(html.read(), "lxml")
+        title = bsObj.body.h1
+    except AttributeError as e:
+        return None
+    return title
+
+
+title = getTitle("http://www.pythonscraping.com/pages/page1.html")
+if title == None:
+    print("Title could not be found")
+else:
+    print(title)
