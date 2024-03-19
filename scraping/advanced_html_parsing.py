0f4468213b95f89fd95102e080190e7cf73227e8
DeSiReLeSsOn <theproblemdi@gmail.com>
2024-03-19 17:42:57 +0300

    retrieve all name in html

--- scraping/advanced_html_parsing.py
+++ scraping/advanced_html_parsing.py
@@ -5,5 +5,15 @@ bs = BeautifulSoup(html, 'html.parser')
 #print(bs)
 
 nameList = bs.find_all('span', {'class': 'green'})
-for name in nameList:
-    print(name.get_text())
+#for name in nameList:
+#    print(name.get_text())
+
+
+
+#titles = bs.find_all(['h1', 'h2','h3','h4','h5','h6'])
+#print([title for title in titles])
+
+
+
+allText = bs.find_all('span', {'class':{'green', 'red'}})
+print([text for text in allText])
