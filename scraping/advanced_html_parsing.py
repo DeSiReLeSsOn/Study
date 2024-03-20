2244b71c3a5395dc5a99db0d5cc8f11a17696988
DeSiReLeSsOn <theproblemdi@gmail.com>
2024-03-20 22:18:12 +0300

    get list with all parent for the tag

--- scraping/advanced_html_parsing.py
+++ scraping/advanced_html_parsing.py
@@ -35,7 +35,22 @@ from bs4 import BeautifulSoup
 примерно два десятка тегов, включая img, span и отдельные
 теги td. 
 """
+#html = urlopen('http://www.pythonscraping.com/pages/page3.html')
+#bs = BeautifulSoup(html, 'html.parser')
+#for child in bs.find('table',{'id':'giftList'}).children:
+#    print(child)
+
+"""Этот код должен выводить все строки таблицы, кроме
+первой с заголовком."""
 html = urlopen('http://www.pythonscraping.com/pages/page3.html')
 bs = BeautifulSoup(html, 'html.parser')
-for child in bs.find('table',{'id':'giftList'}).children:
-    print(child)
+#for sibling in bs.find('table', {'id':'giftList'}).tr.next_siblings:
+#    print(sibling)
+
+"""Этот код будет выводить цену объекта, изображенного на
+картинке ../img/gifts/img1.jpg (в данном случае цена
+составляет 15 долларов).
+"""
+print(bs.find('img', {'src':'../img/gifts/img1.jpg'}).parent.previous_sibling.get_text())
+
+
