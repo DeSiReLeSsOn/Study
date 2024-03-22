80d7bf7cd55c9dfa33d1637a93a09b5be60ee4a8
DeSiReLeSsOn <theproblemdi@gmail.com>
2024-03-22 18:28:24 +0300

    training scraping

--- scraping/web_crawlers.py
+++ scraping/web_crawlers.py
@@ -1,6 +1,9 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import re
+import datetime
+import random
+
 
 """получить произвольную страницу «Википедии» и
 создать список ссылок, присутствующих на этой странице"""
@@ -12,6 +15,24 @@ bs = BeautifulSoup(html, 'html.parser')
 #        print(link.attrs['href'])
 """получим список всех URL статей, на
 которые ссылается статья «Википедии» о Кевине Бейконе"""
-for link in bs.find_all('a', href=re.compile('^(/wiki/)((?!:).)*$')):
-    if 'href' in link.attrs:
-        print(link.attrs['href'])
+#for link in bs.find_all('a', href=re.compile('^(/wiki/)((?!:).)*$')):
+#    if 'href' in link.attrs:
+#       print(link.attrs['href'])
+
+
+"""Сразу после импорта необходимых библиотек программа
+присваивает генератору случайных чисел начальное значение,
+равное текущему системному времени, чем обеспечивает
+новый и интересный случайный путь по статьям «Википедии»
+практически при каждом запуске программы"""
+random.seed(datetime.datetime.now())
+def getLinks(articleUrl):
+    html = urlopen(f'http://en.wikipedia.org{articleUrl}')
+    bs = BeautifulSoup(html, 'html.parser')
+    return bs.find('div', {'id':'bodyContent'}).find_all('a', href=re.compile('^(/wiki/)((?!:).)*$'))
+
+links = getLinks('/wiki/Kevin_Bacon')
+while len(links) > 0:
+    newArticle = links[random.randint(0, len(links)-1)].attrs['href']
+    print(newArticle)
+    links = getLinks(newArticle)
