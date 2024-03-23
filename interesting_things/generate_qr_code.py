54333b89bf5cbc404ed658ad47881181c3d9859c
DeSiReLeSsOn <theproblemdi@gmail.com>
2024-03-23 19:50:15 +0300

    add file who help generate information to qr_code

---
+++ interesting_things/generate_qr_code.py
@@ -0,0 +1,9 @@
+import qrcode
+# пример данных
+data = "wikipedia.com"
+# имя конечного файла
+filename = "site.png"
+# генерируем qr-код
+img = qrcode.make(data)
+# сохраняем img в файл
+img.save(filename)
