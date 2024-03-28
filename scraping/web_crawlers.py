f1b27c21d7a1941b0cfc95eb42185d9930d3e6a4
DeSiReLeSsOn <theproblemdi@gmail.com>
2024-03-29 01:16:54 +0300

    get all link in site with help BeautifulSoup

--- scraping/web_crawlers.py
+++ scraping/web_crawlers.py
@@ -8,8 +8,8 @@ import random
 """получить произвольную страницу «Википедии» и
 создать список ссылок, присутствующих на этой странице"""
 
-html = urlopen('http://en.wikipedia.org/wiki/Kevin_Bacon')
-bs = BeautifulSoup(html, 'html.parser')
+#html = urlopen('http://en.wikipedia.org/wiki/Kevin_Bacon')
+#bs = BeautifulSoup(html, 'html.parser')
 #for link in bs.find_all('a'):
 #    if 'href' in link.attrs:
 #        print(link.attrs['href'])
@@ -25,14 +25,35 @@ bs = BeautifulSoup(html, 'html.parser')
 равное текущему системному времени, чем обеспечивает
 новый и интересный случайный путь по статьям «Википедии»
 практически при каждом запуске программы"""
-random.seed(datetime.datetime.now())
-def getLinks(articleUrl):
-    html = urlopen(f'http://en.wikipedia.org{articleUrl}')
-    bs = BeautifulSoup(html, 'html.parser')
-    return bs.find('div', {'id':'bodyContent'}).find_all('a', href=re.compile('^(/wiki/)((?!:).)*$'))
+#random.seed(datetime.datetime.now())
+#def getLinks(articleUrl):
+#    html = urlopen(f'http://en.wikipedia.org{articleUrl}')
+#    bs = BeautifulSoup(html, 'html.parser')
+#    return bs.find('div', {'id':'bodyContent'}).find_all('a', href=re.compile('^(/wiki/)((?!:).)*$'))
 
-links = getLinks('/wiki/Kevin_Bacon')
-while len(links) > 0:
-    newArticle = links[random.randint(0, len(links)-1)].attrs['href']
-    print(newArticle)
-    links = getLinks(newArticle)
+#links = getLinks('/wiki/Kevin_Bacon')
+#while len(links) > 0:
+#    newArticle = links[random.randint(0, len(links)-1)].attrs['href']
+#    print(newArticle)
+#    links = getLinks(newArticle)
+
+
+"""Веб-скрапер не
+ограничивается только страницами статей, а ищет все ссылки,
+начинающиеся с /wiki/, независимо от того, где они
+располагаются на странице и содержат ли двоеточия."""
+pages = set()
+def getLinks(pageUrl):
+    global pages     
+    html = urlopen('http://en.wikipedia.org{}'.format(pageUrl))
+    bs = BeautifulSoup(html, 'html.parser')
+    for link in bs.find_all('a', href=re.compile('^(/wiki/)')):
+        if 'href' in link.attrs:
+            if link.attrs['href'] not in pages:
+                # мы нашли новую страницу
+                newPage = link.attrs['href']
+                print(newPage)
+                pages.add(newPage)
+                getLinks(newPage) 
+
+getLinks('')
