192e66d0be97c6cc2cdc6e5c377626e60e3f7b2d
DeSiReLeSsOn <theproblemdi@gmail.com>
2024-08-16 18:51:03 +0300

    add crud operations for todo

--- FastApi/todos/model.py
+++ FastApi/todos/model.py
@@ -4,4 +4,14 @@ from pydantic import BaseModel
 
 class Todo(BaseModel):
     id: int 
+    item: str 
+
+
+class Todo_Item(BaseModel):
     item: str
+    class Config:
+        schema_extra = {
+        "example": {
+        "item": "Read the next chapter of the book"
+        }
+}
--- FastApi/todos/todo.py
+++ FastApi/todos/todo.py
@@ -1,5 +1,5 @@
-from fastapi import APIRouter
-from model import Todo
+from fastapi import APIRouter, Path
+from model import Todo, Todo_Item
 
 
 todo_router = APIRouter()
@@ -16,4 +16,49 @@ async def add_todo(todo: Todo) -> dict:
 
 @todo_router.get("/todo")
 async def retrieve_todos() -> dict:
-    return {"todos": todo_list}
+    return {"todos": todo_list} 
+
+
+@todo_router.get("/todo/{todo_id}")
+async def get_single_todo(todo_id: int = Path(..., title="The ID of the todo to retrieve.")) -> dict:
+    for todo in todo_list:
+        if todo.id == todo_id:
+            return {
+                "todo": todo
+            }
+    return {
+        "message": "todo with supplied ID doesn't exist."
+    } 
+
+
+@todo_router.put("/todo/{todo_id}")
+async def update_todo(todo_data: Todo_Item, todo_id: int = Path(..., title="The ID of the todo to be updated")) -> dict:
+    for todo in todo_list:
+        if todo.id == todo_id:
+            todo.item = todo_data.item 
+            return {
+                "message": "Todo updated successfully."
+            }
+    return {
+        "message": "Todo with supplied ID doesn't exist."
+    } 
+
+
+@todo_router.delete("/todo/{todo_id}")
+async def delete_single_todo(todo_id:int) -> dict:
+    for index in range(len(todo_list)):
+        todo = todo_list[index]
+        if todo.id == todo_id:
+            todo_list.pop(index)
+            return {
+                "message": "Todo delete successfully."
+            }
+    return {"message": "Todo with supplied ID doesn't exist."}
+
+
+@todo_router.delete("/todo")
+async def delete_all_todo() -> dict:
+    todo_list.clear()
+    return {
+        "message": "All the todo was removed successfully."
+    }
